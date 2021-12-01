8fbbb9d8fbd378bc390651df16212c7fe36a3f30
deb <lellowgill@gmail.com>
2021-11-30 20:05:54 -0500

    html header for the feature affs button

---
+++ main.js
@@ -0,0 +1,17 @@
+// // onclick of that button update text of the <p>
+// // maybe add an id to the <p> you want to change
+// // then update the text of that id
+
+// let affirmationText = document.getElementById("affirmationText");
+// affirmationText.text("the next word")
+
+
+
+<html>
+    <head>
+        <script>
+        let affirmationText = document.getElementById("affirmationText");
+        affirmationText.text("the next word")
+        </script>
+    </head>
+</html>

aa130cbc2d61733dd6abdcd2b4ba18834d6a785a
deb <lellowgill@gmail.com>
2021-12-01 13:02:24 -0500

    added the function for the affirmation words to change with external javascript

--- main.js
+++ main.js
@@ -5,20 +5,39 @@
 // let affirmationText = document.getElementById("affirmationText");
 // affirmationText.text("the next word")
 
+const affirmationWord = ["grateful.", "thankful.", "thoughtful.", "mindful.", "abundant.", "creative.", 
+    "well rested.", "brave.", "enough.", "respuectful.", "bold.", "vivrant.", "healthy.", "wealthy.",
+    "a gracious receiver.", "gracious giver."];
+const btn = document.getElementById("affirmationBtn");
+const text = document.getElementById("affirmationText");
+let index = 0;
+    btn.addEventListener("click", changeText);
+    text.innerHTML = affirmationWord[0];
+function changeText(){
+    index++;
+    index %= affirmationWord.length
+    text.innerHTML = affirmationWord[index];
+}
+
+//document.getElementById(id).onclick = function (){code}
+
+//let newWord = affirmationWord.length;
+    //for (var i = 0; i < newWord; i++) {
+    //     console.log(newWord);
+    //      return i;
+    // }
 
 
-<html>
-    <head>
-        <script>
-            let affirmationText = document.getElementById("affirmationText");
-            affirmationText.text("the next word")
+// new_arr = affirmationWord.length;
+// console.log(newWord);
 
-            const array_name = ["grateful.", "thankful.", "thoughtful", "mindful", "abundant", "creative", 
-            "well rested", "brave", "enough", "respectful", "bold", "vivrant", "healthy", "wealthy", 
-            "a gracious receiver"]
 
-            <p id="words">Happy</p>
-            <button type="button" onclick="affirmationText()">Happy</button>
-        </script>
-    </head>
-</html>
+
+
+
+
+//for (let i = 0; i < affirmationWord.length; i++) {
+//    console.log(affirmationWord[i]);
+//}
+
+//let affirmationText = document.getElementById("affirmationText");
